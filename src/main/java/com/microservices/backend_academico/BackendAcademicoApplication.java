d8a44b6ef94b82778d92623b20e92a52e9151411
eduardoserpa1 <dudu.rserpa@gmail.com>
2022-09-22 19:25:43 -0300

    adding the basis code to develop a spring-boot aplication with microservice archteture

---
+++ src/main/java/com/microservices/backend_academico/BackendAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.backend_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAcademicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAcademicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservices/backend_academico/BackendAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.backend_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAcademicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
